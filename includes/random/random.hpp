06562cc302e26e5f6fddf69a01399a572263ec38
Illarion <pro100illarion@gmail.com>
2022-05-16 17:45:05 +0300

    Changed and added new function to generate random numbers

--- includes/random/random.hpp
+++ includes/random/random.hpp
@@ -10,8 +10,11 @@
 # include "algorithms/basic_functions/basic_functions.hpp"
 # include "algorithms/check_simple/miller_rabin_test.hpp"
 
-mpz_class	GenerateBigNumber(unsigned int	count_bits);
 mpz_class	GenerateBigNumber(const mpz_class&	max_number);
+mpz_class	GenerateBigNumberInterval(const mpz_class&	min_number, const mpz_class&	max_number);
+mpz_class	GenerateBigNumberInterval(unsigned int	max_bit);
+mpz_class	GenerateBigNumberFixed(unsigned int	bit);
+
 mpz_class 	GenerateSimpleNumber(unsigned int	count_bits_max);
 
 #endif //RSA_RANDOM_HPP
--- srcs/random/random.cpp
+++ srcs/random/random.cpp
@@ -3,6 +3,9 @@
 //
 #include "random/random.hpp"
 
+/*
+	return random mpz_class [0, max_number - 1]
+*/
 mpz_class	GenerateBigNumber(const mpz_class&	max_number)
 {
 	unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
@@ -17,7 +20,18 @@ mpz_class	GenerateBigNumber(const mpz_class&	max_number)
 	return (result);
 }
 
-mpz_class	GenerateBigNumber(unsigned int	count_bits)
+/*
+	return random mpz_class [min_number, max_number]
+*/
+mpz_class	GenerateBigNumberInterval(const mpz_class&	min_number, const mpz_class&	max_number)
+{
+	return (min_number + GenerateBigNumber(max_number - min_number + 1));
+}
+
+/*
+	return random mpz_class [0, 2^max_bit - 1]
+*/
+mpz_class	GenerateBigNumberInterval(unsigned int	max_bit)
 {
 	unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
 	gmp_randstate_t rstate;
@@ -25,9 +39,18 @@ mpz_class	GenerateBigNumber(unsigned int	count_bits)
 	gmp_randseed_ui(rstate, seed);
 	mpz_class	result;
 
-	mpz_urandomb(result.get_mpz_t(), rstate, count_bits);
+	mpz_urandomb(result.get_mpz_t(), rstate, max_bit);
 	return (result);
 }
+
+/*
+	return random mpz_class [2^bit, 2^(bit + 1) - 1]
+*/
+mpz_class	GenerateBigNumberFixed(unsigned int	bit)
+{
+	return ((GenerateBigNumberInterval(bit) | (mpz_class)algorithms::Pow(2, bit)));
+}
+
 
 static bool	FirstSimpleTest(const mpz_class&	number)
 {
@@ -35,14 +58,14 @@ static bool	FirstSimpleTest(const mpz_class&	number)
 			|| number % 7 == 0 || number % 11 == 0);
 }
 
-mpz_class 	GenerateSimpleNumber(unsigned int	count_bits_max)
+mpz_class 	GenerateSimpleNumber(unsigned int	bit)
 {
 
-	mpz_class result = GenerateBigNumber(count_bits_max);
+	mpz_class result = GenerateBigNumberFixed(bit);
 
 	if (result % 2 == 0)
 		++result;
-	while (FirstSimpleTest(result) || !TestMillerRabin(result, 5))
+	while (FirstSimpleTest(result) || !TestMillerRabin(result, bit))
 		result += 2;
 
 	return (result);
