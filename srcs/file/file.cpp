664d8745c62beaafab4bbb43662b1397c816e6f0
Illarion <pro100illarion@gmail.com>
2022-05-16 18:54:58 +0300

    Changed name variable

--- srcs/file/file.cpp
+++ srcs/file/file.cpp
@@ -6,7 +6,7 @@
 std::wstring	BinaryReadUtf8File(const std::string& path)
 {
 	std::wifstream		file;
-	std::wstringstream	sbuff;
+	std::wstringstream	buff;
 
 	if (!std::filesystem::is_regular_file(path))
 		throw std::invalid_argument("path \"" + path + "\" is't a file");
@@ -16,14 +16,14 @@ std::wstring	BinaryReadUtf8File(const std::string& path)
 		throw std::ifstream::failure("file \"" + path + "\" is't open");
 	file.imbue(std::locale(std::locale(), new std::codecvt_utf8<wchar_t>));
 
-	sbuff << file.rdbuf();
+	buff << file.rdbuf();
 
 	if (!file)
 		throw std::ifstream::failure("only " + std::to_string(file.gcount()) + " byte could be read");
 
 	file.close();
 
-	return (sbuff.str());
+	return (buff.str());
 }
 
 void	BinaryWriteUtf8File(const std::string& path, const std::wstring&	buff)
